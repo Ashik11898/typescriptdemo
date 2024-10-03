02eec9db615533bbe139757b0690971d8c2d234c
Ashik11898 <mohamed.ashik@broadbil.com>
2024-10-03 21:06:43 +0400

    navbar structure portfolio done

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,3 +1,4 @@
+import {useEffect,useRef,useState} from "react"
 import { ParaHead, ParaText } from "../elements/HeadingText"
 import { PiTwitterLogoBold } from "react-icons/pi";
 import { PiInstagramLogoBold } from "react-icons/pi";
@@ -10,13 +11,22 @@ type ProfileProps={
   subdesc:string
 }
 
-
-
 const ProfileCard = ({name,subdesc}:ProfileProps) => {
+
+  const [windowInnerht,setWindowInnerht]=useState<number | null>(null)
+  const profileBlock = useRef(null)
+
+  useEffect(() => {
+   setWindowInnerht(window.innerHeight)
+  }, [])
+
+console.log("w",windowInnerht);
+
+
   return (
-    <div className='profile-parent'>
+    <div className='profile-parent'   style={{ height: windowInnerht !== null && windowInnerht > 580 ? "550px" : "" }}>
       <div className='profile-imgblock'></div>
-      <div className='profile-name'><ParaHead mainText={name} styles={PARAHEAD_STYLE1}/></div>
+      <div className='profile-name'><ParaHead mainText={name}/></div>
       <div className='profile-subdesc'><ParaText mainText={subdesc}/></div>
       <div className='profile-icons'>
         <PiTwitterLogoBold color={ICONDETAILS.color} size={ICONDETAILS.size}/>
